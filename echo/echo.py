48d191e44f04fb11a3ed742b3e74129ad10d2368
Krissbro <Krissbro@me.com>
2017-01-22 13:14:33 +0000

    Added Echo cog

---
+++ echo/echo.py
@@ -0,0 +1,38 @@
+from cogs.utils import checks
+from discord.ext import commands
+
+
+class Echo:
+    """I'll repeat what you said."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.is_owner()
+    async def echo(self, *text):
+        """I'll repeat what you said."""
+
+        text = " ".join(text)
+        await self.bot.say(text)
+
+    @commands.command()
+    @checks.is_owner()
+    async def sonar(self, channelid, *, text):
+        """I'll repeat what you said and where you want it.
+        A modified version of the debug command, with help from Calebj."""
+
+        text = text.replace("\'", "\\\'")
+        channelid = self.bot.get_channel(str(channelid))
+        return await self.bot.send_message(channelid, text)
+
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def channelidget(self, ctx):
+        """Gets current channel ID immediately."""
+        channel = ctx.message.channel
+        return await self.bot.say(channel.id)
+
+
+def setup(bot):
+    bot.add_cog(Echo(bot))
