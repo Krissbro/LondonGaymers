1449638683774ade96adf02ec0f8829d143b970d
Krissbro <Krissbro@me.com>
2017-01-22 13:00:24 +0000

    Added Avatar command

---
+++ getavatar/getavatar.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+
+class Avatar:
+    """Get user's avatar"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def getavatar(self, ctx, *, user: discord.Member=None):
+        author = ctx.message.author
+
+        if not user:
+            user = author
+
+        avatar = user.avatar_url .strip("webp?size=1024")
+        await self.bot.say(":eyes: {} | {}'s Avatar is : {}jpg".format(author.mention, user.mention, avatar))
+
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
