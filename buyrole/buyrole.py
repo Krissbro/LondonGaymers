a9726f3be78e67c0eb9c06db20b7e1f3fe441a98
Krissbro <Krissbro@me.com>
2017-01-22 20:41:14 +0000

    uodate

--- buyrole/buyrole.py
+++ buyrole/buyrole.py
@@ -32,13 +32,20 @@ class Buyrole:
         self.settings_dict = dataIO.load_json(self.settings_loc)
 
     @commands.command(pass_context=True, aliases=['requestrole'], no_pm=True)
-    async def games(self, ctx, *, role: str = None):
-        """Join game-related roles on the server. 
-		to see a list roles type ``games``"""
+    async def buyrole(self, ctx, *, role: str = None):
+        """Buy roles with economy credits,
+        To see the list of roles you can buy use ``buyrole``"""
+        server = ctx.message.server
+        str_role = role
+        role = discord.utils.get(server.roles, name=str_role)
+        if role is None:
+            await self.bot.say('I cannot find the role you\'re trying to buy.\n'
+                               'Please make sure that you\'ve capitalised the role name.')
+            return
         if server.id not in self.settings_dict:
             await self.bot.say('This server doesn\'t have a shop yet')
         elif role in ctx.message.author.roles:
-            await self.bot.say('You already have this role.')
+            await self.bot.say('You already own this role.')
         elif 'Economy' not in self.bot.cogs:
             await self.bot.say('Economy isn\'t loaded. Please load economy.')
         elif role is None:
@@ -57,8 +64,8 @@ class Buyrole:
 
     @commands.group(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(manage_roles=True)
-    async def gameset(self, ctx):
-        """Manage games"""
+    async def buyroleset(self, ctx):
+        """Manage buyrole"""
         if 'Economy' not in self.bot.cogs:
             raise RuntimeError('The Economy cog needs to be loaded for this cog to work')
         server = ctx.message.server
@@ -68,9 +75,9 @@ class Buyrole:
         if ctx.invoked_subcommand is None:
             await self.bot.send_cmd_help(ctx)
 
-    @gameset.command(pass_context=True, no_pm=True, aliases=['edit'])
+    @buyroleset.command(pass_context=True, no_pm=True, aliases=['edit'])
     async def add(self, ctx, role: discord.Role, price: int):
-        """Add a role for users to join"""
+        """Add a role for users to buy"""
         server = ctx.message.server
         if price < 0:
             await self.bot.say('The price cannot be below 0. To make it free use 0 as the price.')  # In command error handling, no excetion due the rarity.
@@ -79,12 +86,12 @@ class Buyrole:
             self.settings_dict[server.id]['roles'][role.id]['price'] = price
         else:
             self.settings_dict[server.id]['roles'][role.id] = {'price': price, 'uniquegroup': 0}
-            await self.bot.say('{0} added to the games list. The price of {0} is now {1}.'.format(role.name, self._price_string(price, False)))
+            await self.bot.say('{0} added to the buyrole list. The price of {0} is now {1}.'.format(role.name, self._price_string(price, False)))
         self.save_json()
 
-    @gameset.command(pass_context=True, no_pm=True)
+    @buyroleset.command(pass_context=True, no_pm=True)
     async def remove(self, ctx, role: discord.Role):
-        """Remove a role for users to join"""
+        """Remove a role for users to buy"""
         server = ctx.message.server
         try:
             del self.settings_dict[server.id]['roles'][role.id]
@@ -93,31 +100,31 @@ class Buyrole:
         except:
             await self.bot.say('This role isn\'t in the list.')
 
-    @gameset.command(pass_context=True, no_pm=True)
+    @buyroleset.command(pass_context=True, no_pm=True)
     async def toggle(self, ctx, toggle: bool):
-        """Open or close the Games list
+        """Open or close the buyrole shop
 
         Use either True or False
-        gameset toggle true"""
+        buyroleset toggle true"""
         server = ctx.message.server
         if toggle is True:
             if self.settings_dict[server.id]['toggle'] is True:
-                await self.bot.say('The list is already enabled')
+                await self.bot.say('The shop is already enabled')
             else:
                 self.settings_dict[server.id]['toggle'] = True
                 self.save_json()
-                await self.bot.say('The list has been enabled.')
+                await self.bot.say('The shop has been enabled.')
         elif toggle is False:
             if self.settings_dict[server.id]['toggle'] is False:
-                await self.bot.say('The list is already disabled')
+                await self.bot.say('The shop is already disabled')
             else:
                 self.settings_dict[server.id]['toggle'] = False
                 self.save_json()
-                await self.bot.say('The list has been disabled')
+                await self.bot.say('The shop has been disabled')
         else:
             raise Exception('InvalidToggle')
 
-    @gameset.command(pass_context=True, no_pm=True)
+    @buyroleset.command(pass_context=True, no_pm=True)
     async def uniquegroup(self, ctx, role: discord.Role, groupid: int):
         """Set a role to a unique group ID,
         This means that a user cannot have more then one role from the same group.
@@ -138,9 +145,9 @@ class Buyrole:
             else:
                 await self.bot.say('Unique Group ID set. {} will now be unique in group ID {}'.format(role.name, groupid))
 
-    @gameset.command(pass_context=True, no_pm=True, aliases=['color'])
+    @buyroleset.command(pass_context=True, no_pm=True, aliases=['color'])
     async def colour(self, ctx, colour: discord.Colour):
-        """Set the sidebar colour in the games list."""
+        """Set the sidebar colour in the buyrole list."""
         server = ctx.message.server
         self.settings_dict[server.id]['colour'] = colour.value
         self.save_json()
@@ -170,9 +177,9 @@ class Buyrole:
 
     def _price_string(self, price, punctuation: bool):
         if price == 0 and punctuation is True:
-            return ":heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: "
+            return "Free!"
         elif price == 0 and punctuation is False:
-            return ":heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: "
+            return "free"
         else:
             return str(price)
 
