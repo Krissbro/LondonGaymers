db0b387b7b06367f7416788014b6b1f9fc8dbe5b
Krissbro <Krissbro@me.com>
2017-01-27 10:01:24 +0000

    Fixed

--- tools/tools.py
+++ tools/tools.py
@@ -207,6 +207,34 @@ class Tools:
         settings['TIME_API_KEY'] = key
         dataIO.save_json(self.settings_file, settings)
 
+   @commands.command(pass_context=True)
+    async def emoji(self, ctx, name: str):
+        """Send a large custom emoji. 
+        Bot must be in the server with the emoji"""
+        for x in list(self.bot.get_all_emojis()):
+            if x.name.lower() == name.lower():
+                fdir ="data/moji/" + x.server.name
+                fp = fdir + "/{0.name}.png".format(x)
+                if not os.path.exists(fdir):
+                    os.mkdir(fdir)
+                if not os.path.isfile(fp):
+                    async with aiohttp.get(x.url) as r:
+                        img_bytes = await r.read()
+                        img = io.BytesIO(img_bytes)
+                        with open(fp, 'wb') as o:
+                            o.write(img.read())
+                        o.close()
+
+#You can uncomment this line if you want c: 
+                #await self.bot.delete_message(ctx.message)
+                return await self.bot.send_file(ctx.message.channel, fp)
+
+    @commands.group(pass_context=True)
+    async def moji(self, ctx):
+        """Various emoji operations"""
+        if ctx.invoked_subcommand is None:
+            return await send_cmd_help(ctx)
+
     @moji.command(pass_context=True)
     async def list(self, ctx, server: int = None):
         """List all available custom emoji"""
