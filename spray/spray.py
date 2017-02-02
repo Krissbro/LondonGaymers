93f107d06a49928d034940a6537d14126a432fa6
Krissbro <Krissbro@me.com>
2017-02-02 14:54:00 +0000

    changed Boy to Bot...

--- spray/spray.py
+++ spray/spray.py
@@ -44,7 +44,7 @@ class Spray:
             await self.bot.say("You haven't told me who to spray, so I guess I'll spray you {}! {}".format (author.mention, choice(self.spraylist)))
             return
         elif len(message.mentions) == +1:
-            await self.boy.say("Multi-Spray!! {} has just sprayed {}!! \n".format(author.mention, message.mentions, choice(self.spraylist)))
+            await self.bot.say("Multi-Spray!! {} has just sprayed {}!! \n".format(author.mention, message.mentions, choice(self.spraylist)))
             return
 
         await self.bot.say("{} you have just sprayed {}\n{}".format(author.mention, user.mention, choice(self.spraylist)))
