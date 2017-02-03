7fbac43a1b40ed27645db3fbf44cf7fb94ebe30d
Krissbro <Krissbro@me.com>
2017-02-03 10:09:26 +0000

    removed =None

--- spray/spray.py
+++ spray/spray.py
@@ -35,7 +35,7 @@ class Spray:
         self.spraylist = dataIO.load_json("data/spray/sprays.json")
         
     @commands.command(pass_context=True)
-    async def spray(self, ctx, *, user: discord.Member=None,):
+    async def spray(self, ctx, *, user: discord.Member,):
         """Lets you spray someone."""
 
         message = ctx.message
