dcc069be5d1ed752dc67a75d1be7b7cd13b6ef3a
Brendan <tarrnobi@users.noreply.github.com>
2017-02-04 10:15:54 +0000

    Re-enabled code

--- overwatch/overwatch.py
+++ overwatch/overwatch.py
@@ -46,83 +46,83 @@ class Overwatch:
         await self.bot.say("requests: {0}".format(requests))
         responses = grequests.map(requests)
         await self.bot.say("responses: {0}".format(responses))
-        # if responses[0].status_code == 200 and responses[1].status_code == 200 and responses[2].status_code == 200:
-        #     user_data     = responses[0].json()
-        #     comp_heroes   = responses[1].json()
-        #     quick_heroes  = responses[2].json()
-        #
-        #     # Build our hero break downs for competitive and quick play.
-        #     # We'll take the top five heroes and work out their percentage playtime
-        #     # of the total.
-        #     cp_hero_breakdown = {}
-        #     cp_tot_hero_playtime = float(0)
-        #     for item in comp_heroes:
-        #         hero_name = item.get("name").replace("&#xFA;","u")
-        #         if "minutes" in item.get("playtime"):
-        #             playtime = float(item.get("playtime").replace("minutes",""))/60
-        #         elif "hours" in item.get("playtime"):
-        #             playtime = float(item.get("playtime").replace("hours",""))
-        #
-        #         cp_tot_hero_playtime += playtime
-        #
-        #         cp_hero_breakdown[hero_name] = playtime
-        #         if len(cp_hero_breakdown.keys()) >= 5:
-        #             break
-        #     qp_hero_breakdown = {}
-        #     qp_tot_hero_playtime = float(0)
-        #     # Repeat the above process for quick play.
-        #     for item in quick_heroes:
-        #         hero_name = item.get("name").replace("&#xFA;","u")
-        #         if "minutes" in item.get("playtime"):
-        #             playtime = float(item.get("playtime").replace("minutes",""))/60
-        #         elif "hours" in item.get("playtime"):
-        #             playtime = float(item.get("playtime").replace("hours",""))
-        #
-        #         qp_tot_hero_playtime += playtime
-        #
-        #         qp_hero_breakdown[hero_name] = playtime
-        #         if len(qp_hero_breakdown.keys()) >= 5:
-        #             break
-        #     #Now we can build our message to have the bot say.
-        #     msg  = "```\n"
-        #     msg += "--------------\n"
-        #     msg += "User : {0}\n".format(user_data["data"]["username"])
-        #     msg += "Level: {0}\n".format(user_data["data"]["level"])
-        #     msg += "--------------\n"
-        #     msg += "             Played|Wins  |losses|Time\n"
-        #     msg += "Quick Play : {0}|{1}|{2}|{3}\n".format( str(user_data.get("data").get("games").get("quick").get("played","0")).ljust(6),
-        #                                                     str(user_data.get("data").get("games").get("quick").get("wins","0")).ljust(6),
-        #                                                     str(user_data.get("data").get("games").get("quick").get("lost","0")).ljust(6),
-        #                                                     str(user_data.get("data").get("playtime").get("quick","0")))
-        #     msg += "Competitive: {0}|{1}|{2}|{3}\n".format( str(user_data.get("data").get("games").get("competitive").get("played","0")).ljust(6),
-        #                                                     str(user_data.get("data").get("games").get("competitive").get("wins","0")).ljust(6),
-        #                                                     str(user_data.get("data").get("games").get("competitive").get("lost","0")).ljust(6),
-        #                                                     str(user_data.get("data").get("playtime").get("competitive","0")))
-        #
-        #     msg += "------------------------------\n"
-        #     msg += "Competitive Hero Breakdown\n"
-        #     msg += "------------------------------\n"
-        #     for name, playtime in cp_hero_breakdown.items():
-        #         if cp_tot_hero_playtime > 0:
-        #             percentage = round((playtime / cp_tot_hero_playtime * 100),0)
-        #         else:
-        #             percentage = 0
-        #         msg += "{0} [{1}]\n".format(name.ljust(12), ("|" * int(math.ceil(percentage/10))).ljust(10))
-        #     msg += "------------------------------\n"
-        #     msg += "QuickPlay Hero Breakdown\n"
-        #     msg += "------------------------------\n"
-        #     for name, playtime in qp_hero_breakdown.items():
-        #         if qp_tot_hero_playtime > 0:
-        #             percentage = round((playtime / qp_tot_hero_playtime * 100),0)
-        #         else:
-        #             percentage = 0
-        #         msg += "{0} [{1}]\n".format(name.ljust(12), ("|" * int(math.ceil(percentage/10))).ljust(10))
-        #     msg += "```"
-        #
-        #     # Now instruct the bot to say the message.
-        #     await self.bot.say(msg)
-        # else:
-        #     await self.bot.say("Sorry, I could not find the battle-tag {0}".format(battle_tag))
+        if responses[0].status_code == 200 and responses[1].status_code == 200 and responses[2].status_code == 200:
+            user_data     = responses[0].json()
+            comp_heroes   = responses[1].json()
+            quick_heroes  = responses[2].json()
+
+            # Build our hero break downs for competitive and quick play.
+            # We'll take the top five heroes and work out their percentage playtime
+            # of the total.
+            cp_hero_breakdown = {}
+            cp_tot_hero_playtime = float(0)
+            for item in comp_heroes:
+                hero_name = item.get("name").replace("&#xFA;","u")
+                if "minutes" in item.get("playtime"):
+                    playtime = float(item.get("playtime").replace("minutes",""))/60
+                elif "hours" in item.get("playtime"):
+                    playtime = float(item.get("playtime").replace("hours",""))
+
+                cp_tot_hero_playtime += playtime
+
+                cp_hero_breakdown[hero_name] = playtime
+                if len(cp_hero_breakdown.keys()) >= 5:
+                    break
+            qp_hero_breakdown = {}
+            qp_tot_hero_playtime = float(0)
+            # Repeat the above process for quick play.
+            for item in quick_heroes:
+                hero_name = item.get("name").replace("&#xFA;","u")
+                if "minutes" in item.get("playtime"):
+                    playtime = float(item.get("playtime").replace("minutes",""))/60
+                elif "hours" in item.get("playtime"):
+                    playtime = float(item.get("playtime").replace("hours",""))
+
+                qp_tot_hero_playtime += playtime
+
+                qp_hero_breakdown[hero_name] = playtime
+                if len(qp_hero_breakdown.keys()) >= 5:
+                    break
+            #Now we can build our message to have the bot say.
+            msg  = "```\n"
+            msg += "--------------\n"
+            msg += "User : {0}\n".format(user_data["data"]["username"])
+            msg += "Level: {0}\n".format(user_data["data"]["level"])
+            msg += "--------------\n"
+            msg += "             Played|Wins  |losses|Time\n"
+            msg += "Quick Play : {0}|{1}|{2}|{3}\n".format( str(user_data.get("data").get("games").get("quick").get("played","0")).ljust(6),
+                                                            str(user_data.get("data").get("games").get("quick").get("wins","0")).ljust(6),
+                                                            str(user_data.get("data").get("games").get("quick").get("lost","0")).ljust(6),
+                                                            str(user_data.get("data").get("playtime").get("quick","0")))
+            msg += "Competitive: {0}|{1}|{2}|{3}\n".format( str(user_data.get("data").get("games").get("competitive").get("played","0")).ljust(6),
+                                                            str(user_data.get("data").get("games").get("competitive").get("wins","0")).ljust(6),
+                                                            str(user_data.get("data").get("games").get("competitive").get("lost","0")).ljust(6),
+                                                            str(user_data.get("data").get("playtime").get("competitive","0")))
+
+            msg += "------------------------------\n"
+            msg += "Competitive Hero Breakdown\n"
+            msg += "------------------------------\n"
+            for name, playtime in cp_hero_breakdown.items():
+                if cp_tot_hero_playtime > 0:
+                    percentage = round((playtime / cp_tot_hero_playtime * 100),0)
+                else:
+                    percentage = 0
+                msg += "{0} [{1}]\n".format(name.ljust(12), ("|" * int(math.ceil(percentage/10))).ljust(10))
+            msg += "------------------------------\n"
+            msg += "QuickPlay Hero Breakdown\n"
+            msg += "------------------------------\n"
+            for name, playtime in qp_hero_breakdown.items():
+                if qp_tot_hero_playtime > 0:
+                    percentage = round((playtime / qp_tot_hero_playtime * 100),0)
+                else:
+                    percentage = 0
+                msg += "{0} [{1}]\n".format(name.ljust(12), ("|" * int(math.ceil(percentage/10))).ljust(10))
+            msg += "```"
+
+            # Now instruct the bot to say the message.
+            await self.bot.say(msg)
+        else:
+            await self.bot.say("Sorry, I could not find the battle-tag {0}".format(battle_tag))
 
 def setup(bot):
     if not grequestsAvailable:
