047e039a6eac0b59d7731881c28ebd256d517ccc
Krissbro <Krissbro@me.com>
2017-03-03 09:05:02 +0000

    tabs and spaces

--- getavatar/getavatar.py
+++ getavatar/getavatar.py
@@ -16,7 +16,7 @@ class Avatar:
         if not user:
             user = author
         
-		avatar = user.avatar_url .strip("webp?size=1024")
+        avatar = user.avatar_url .strip("webp?size=1024")
         if "gif" not in avatar:
             await self.bot.say(":eyes: {} | {}'s Avatar is : {}jpg".format(author.mention, user.mention, avatar))
         else:
